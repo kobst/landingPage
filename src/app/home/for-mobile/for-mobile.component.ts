584a63a73f4dbbef0a26dd2c82926766ba994df0
kobst <edwrdhn@gmail.com>
2020-04-08 21:45:22 -0400

    first version

---
+++ src/app/home/for-mobile/for-mobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-for-mobile',
+  templateUrl: './for-mobile.component.html',
+  styleUrls: ['./for-mobile.component.scss']
+})
+export class ForMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/home/intro-plazm/intro-plazm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-plazm',
+  templateUrl: './intro-plazm.component.html',
+  styleUrls: ['./intro-plazm.component.scss']
+})
+export class IntroPlazmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
