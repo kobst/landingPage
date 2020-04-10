8405cca989c1ce99cdf9ac05c596ebd5ab75d25f
kobst <edwrdhn@gmail.com>
2020-04-10 15:24:37 -0400

    removed a lot of extra files. Working but adding 'main' to path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import {  HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { BlogComponent } from './blog/blog.component';
+import { ManifestoComponent } from './manifesto/manifesto.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent,
-    BlogComponent
+    ManifestoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
