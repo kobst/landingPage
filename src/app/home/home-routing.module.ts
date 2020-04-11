95e379862862d8ad85c6d03576a48d49f3379c73
kobst <edwrdhn@gmail.com>
2020-04-10 20:51:46 -0400

    removed /main from routes

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     path: '',
     children: [
       {
-        path: 'main',
+        path: '',
         component: HomeOneComponent,
         data: {
           title: 'Introduction'
